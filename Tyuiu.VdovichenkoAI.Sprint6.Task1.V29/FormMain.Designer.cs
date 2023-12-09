c9165f9a758b66c22cf3da02ca780e02ea5f09a9
Tyuiu.VdovichenkoAI@outlook.com <a.vdovichenko10@gmail.com>
2023-12-09 20:20:04 +0500

    Task1

---
+++ Tyuiu.VdovichenkoAI.Sprint6.Task1.V29/FormMain.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Tyuiu.VdovichenkoAI.Sprint6.Task1.V29
+{
+    partial class FormMain_VAI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxTask1_VAI = new System.Windows.Forms.GroupBox();
+            this.textBoxTask1_VAI = new System.Windows.Forms.TextBox();
+            this.groupBoxResult_VAI = new System.Windows.Forms.GroupBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxResult_VAI = new System.Windows.Forms.TextBox();
+            this.groupBoxInPutData_VAI = new System.Windows.Forms.GroupBox();
+            this.labelStart_VAI = new System.Windows.Forms.Label();
+            this.labelFinal_VAI = new System.Windows.Forms.Label();
+            this.textBoxStart_VAI = new System.Windows.Forms.TextBox();
+            this.textBoxFinal_VAI = new System.Windows.Forms.TextBox();
+            this.buttonInfo_VAI = new System.Windows.Forms.Button();
+            this.buttonDone_VAI = new System.Windows.Forms.Button();
+            this.groupBoxTask1_VAI.SuspendLayout();
+            this.groupBoxResult_VAI.SuspendLayout();
+            this.groupBoxInPutData_VAI.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxTask1_VAI
+            // 
+            this.groupBoxTask1_VAI.Controls.Add(this.textBoxTask1_VAI);
+            this.groupBoxTask1_VAI.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxTask1_VAI.Name = "groupBoxTask1_VAI";
+            this.groupBoxTask1_VAI.Size = new System.Drawing.Size(620, 294);
+            this.groupBoxTask1_VAI.TabIndex = 0;
+            this.groupBoxTask1_VAI.TabStop = false;
+            this.groupBoxTask1_VAI.Text = "Условие";
+            // 
+            // textBoxTask1_VAI
+            // 
+            this.textBoxTask1_VAI.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxTask1_VAI.Location = new System.Drawing.Point(7, 21);
+            this.textBoxTask1_VAI.Multiline = true;
+            this.textBoxTask1_VAI.Name = "textBoxTask1_VAI";
+            this.textBoxTask1_VAI.ReadOnly = true;
+            this.textBoxTask1_VAI.Size = new System.Drawing.Size(607, 267);
+            this.textBoxTask1_VAI.TabIndex = 0;
+            this.textBoxTask1_VAI.TabStop = false;
+            this.textBoxTask1_VAI.Text = "Произвести табулирование функции f(x) на заданном диапазоне. Результат вывести в " +
+    "виде таблицы.\r\n";
+            // 
+            // groupBoxResult_VAI
+            // 
+            this.groupBoxResult_VAI.Controls.Add(this.textBoxResult_VAI);
+            this.groupBoxResult_VAI.Controls.Add(this.labelResult);
+            this.groupBoxResult_VAI.Location = new System.Drawing.Point(639, 13);
+            this.groupBoxResult_VAI.Name = "groupBoxResult_VAI";
+            this.groupBoxResult_VAI.Size = new System.Drawing.Size(337, 425);
+            this.groupBoxResult_VAI.TabIndex = 1;
+            this.groupBoxResult_VAI.TabStop = false;
+            this.groupBoxResult_VAI.Text = "Вывод данных";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(7, 22);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(80, 17);
+            this.labelResult.TabIndex = 0;
+            this.labelResult.Text = "Результат:";
+            // 
+            // textBoxResult_VAI
+            // 
+            this.textBoxResult_VAI.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxResult_VAI.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxResult_VAI.Location = new System.Drawing.Point(7, 43);
+            this.textBoxResult_VAI.Multiline = true;
+            this.textBoxResult_VAI.Name = "textBoxResult_VAI";
+            this.textBoxResult_VAI.ReadOnly = true;
+            this.textBoxResult_VAI.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResult_VAI.Size = new System.Drawing.Size(324, 376);
+            this.textBoxResult_VAI.TabIndex = 1;
+            this.textBoxResult_VAI.TabStop = false;
+            // 
+            // groupBoxInPutData_VAI
+            // 
+            this.groupBoxInPutData_VAI.Controls.Add(this.textBoxFinal_VAI);
+            this.groupBoxInPutData_VAI.Controls.Add(this.textBoxStart_VAI);
+            this.groupBoxInPutData_VAI.Controls.Add(this.labelFinal_VAI);
+            this.groupBoxInPutData_VAI.Controls.Add(this.labelStart_VAI);
+            this.groupBoxInPutData_VAI.Location = new System.Drawing.Point(13, 313);
+            this.groupBoxInPutData_VAI.Name = "groupBoxInPutData_VAI";
+            this.groupBoxInPutData_VAI.Size = new System.Drawing.Size(313, 125);
+            this.groupBoxInPutData_VAI.TabIndex = 2;
+            this.groupBoxInPutData_VAI.TabStop = false;
+            this.groupBoxInPutData_VAI.Text = "Ввод данных";
+            // 
+            // labelStart_VAI
+            // 
+            this.labelStart_VAI.AutoSize = true;
+            this.labelStart_VAI.Location = new System.Drawing.Point(6, 42);
+            this.labelStart_VAI.Name = "labelStart_VAI";
+            this.labelStart_VAI.Size = new System.Drawing.Size(87, 17);
+            this.labelStart_VAI.TabIndex = 0;
+            this.labelStart_VAI.Text = "Старт шага:";
+            // 
+            // labelFinal_VAI
+            // 
+            this.labelFinal_VAI.AutoSize = true;
+            this.labelFinal_VAI.Location = new System.Drawing.Point(168, 42);
+            this.labelFinal_VAI.Name = "labelFinal_VAI";
+            this.labelFinal_VAI.Size = new System.Drawing.Size(89, 17);
+            this.labelFinal_VAI.TabIndex = 1;
+            this.labelFinal_VAI.Text = "Конец шага:";
+            // 
+            // textBoxStart_VAI
+            // 
+            this.textBoxStart_VAI.Location = new System.Drawing.Point(6, 76);
+            this.textBoxStart_VAI.Name = "textBoxStart_VAI";
+            this.textBoxStart_VAI.Size = new System.Drawing.Size(118, 22);
+            this.textBoxStart_VAI.TabIndex = 2;
+            // 
+            // textBoxFinal_VAI
+            // 
+            this.textBoxFinal_VAI.Location = new System.Drawing.Point(171, 76);
+            this.textBoxFinal_VAI.Name = "textBoxFinal_VAI";
+            this.textBoxFinal_VAI.Size = new System.Drawing.Size(118, 22);
+            this.textBoxFinal_VAI.TabIndex = 3;
+            // 
+            // buttonInfo_VAI
+            // 
+            this.buttonInfo_VAI.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonInfo_VAI.Location = new System.Drawing.Point(332, 319);
+            this.buttonInfo_VAI.Name = "buttonInfo_VAI";
+            this.buttonInfo_VAI.Size = new System.Drawing.Size(112, 119);
+            this.buttonInfo_VAI.TabIndex = 3;
+            this.buttonInfo_VAI.Text = "Справка";
+            this.buttonInfo_VAI.UseVisualStyleBackColor = false;
+            this.buttonInfo_VAI.Click += new System.EventHandler(this.buttonInfo_VAI_Click);
+            // 
+            // buttonDone_VAI
+            // 
+            this.buttonDone_VAI.BackColor = System.Drawing.Color.ForestGreen;
+            this.buttonDone_VAI.Location = new System.Drawing.Point(451, 319);
+            this.buttonDone_VAI.Name = "buttonDone_VAI";
+            this.buttonDone_VAI.Size = new System.Drawing.Size(181, 119);
+            this.buttonDone_VAI.TabIndex = 4;
+            this.buttonDone_VAI.Text = "Выполнить";
+            this.buttonDone_VAI.UseVisualStyleBackColor = false;
+            this.buttonDone_VAI.Click += new System.EventHandler(this.buttonDone_VAI_Click);
+            // 
+            // FormMain_VAI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 450);
+            this.Controls.Add(this.buttonDone_VAI);
+            this.Controls.Add(this.buttonInfo_VAI);
+            this.Controls.Add(this.groupBoxInPutData_VAI);
+            this.Controls.Add(this.groupBoxResult_VAI);
+            this.Controls.Add(this.groupBoxTask1_VAI);
+            this.Name = "FormMain_VAI";
+            this.Text = "Спринт 6 | Таск 1 | Вариант 29 | Вдовиченко А. И.";
+            this.groupBoxTask1_VAI.ResumeLayout(false);
+            this.groupBoxTask1_VAI.PerformLayout();
+            this.groupBoxResult_VAI.ResumeLayout(false);
+            this.groupBoxResult_VAI.PerformLayout();
+            this.groupBoxInPutData_VAI.ResumeLayout(false);
+            this.groupBoxInPutData_VAI.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxTask1_VAI;
+        private System.Windows.Forms.TextBox textBoxTask1_VAI;
+        private System.Windows.Forms.GroupBox groupBoxResult_VAI;
+        private System.Windows.Forms.TextBox textBoxResult_VAI;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.GroupBox groupBoxInPutData_VAI;
+        private System.Windows.Forms.TextBox textBoxFinal_VAI;
+        private System.Windows.Forms.TextBox textBoxStart_VAI;
+        private System.Windows.Forms.Label labelFinal_VAI;
+        private System.Windows.Forms.Label labelStart_VAI;
+        private System.Windows.Forms.Button buttonInfo_VAI;
+        private System.Windows.Forms.Button buttonDone_VAI;
+    }
+}
+
---
+++ Tyuiu.VdovichenkoAI.Sprint6.Task1.V29/FormMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.VdovichenkoAI.Sprint6.Task1.V29.Lib;
+
+namespace Tyuiu.VdovichenkoAI.Sprint6.Task1.V29
+{
+    public partial class FormMain_VAI : Form
+    {
+        public FormMain_VAI()
+        {
+            InitializeComponent();
+        }
+        DataService ds = new DataService();
+        private void buttonDone_VAI_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int startValue = Convert.ToInt32(textBoxStart_VAI.Text);
+                int stopValue = Convert.ToInt32(textBoxFinal_VAI.Text);
+
+                string strLine;
+
+                int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+                double[] valueArray = new double[len];
+
+                valueArray = ds.GetMassFunction(startValue, stopValue);
+                textBoxResult_VAI.Text = "";
+                textBoxResult_VAI.AppendText("+----------+----------+" + Environment.NewLine);
+                textBoxResult_VAI.AppendText("|    X     |    f(x)  |" + Environment.NewLine);
+                textBoxResult_VAI.AppendText("+----------+----------+" + Environment.NewLine);
+
+                for (int i = 0; i <= len - 1; i++)
+                {
+                    strLine = String.Format("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+                    textBoxResult_VAI.AppendText(strLine + Environment.NewLine);
+                    startValue++;
+                }
+                textBoxResult_VAI.AppendText("+----------+----------+" + Environment.NewLine);
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonInfo_VAI_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 1 выполнила студентка группы ИИПб-23-1 Вдовиченко Алина Игоревна", "Сообщение");
+        }
+    }
+}
